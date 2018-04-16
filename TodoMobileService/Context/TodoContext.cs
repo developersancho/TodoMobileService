d4344f4045bd90eb646cf8ba259b059a44e9a241
developersancho <developersancho@gmail.com>
2018-04-16 13:59:29 +0300

    ---
    ---

---
+++ TodoMobileService/Context/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoMobileService.Models;
+
+namespace TodoMobileService.Context
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+    }
+}
